df36ce2c160a79120e10c8f23eb3825d20e5767d
Laurence Nunn <lnunn279@gmail.com>
2024-10-03 23:13:08 +0100

    Improve nav item styling

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,22 +1,28 @@
-import { FC } from "react"
+import { FC, useEffect } from "react"
 import styles from "./Navbar.module.css"
 import { NavItem } from "@/types/navItem";
+import { useRouter } from "next/router";
 
 interface Props {
     navItems: NavItem[];
 }
 
 export const Navbar: FC<Props> = ({ navItems }) => {
+    const router = useRouter();
     return (
         <>
             <nav className={styles.navBar}>
                 <ul>
                     {navItems.map(({ url, icon, title }) => {
-                        return <li className={styles.navItem} key={title}>
+                        return <li key={title}>
+                            <a className={`${styles.navItem} ${router.asPath === url ? styles.isActive : ''}`} href={url}>
                             <span>
                                 {icon}
                             </span>
-                            <a href={url}>{title}</a>
+                            <span>
+                                {title}
+                            </span>
+                            </a>
                         </li>
                     })}
                 </ul>
